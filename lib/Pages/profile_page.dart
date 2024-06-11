f8d926570d94edea34add3787f95665da9dbb3ef
AhsinMehmood <60714982+AhsinMehmood@users.noreply.github.com>
2024-06-11 10:09:06 +0500

    ,,

--- lib/Pages/profile_page.dart
+++ lib/Pages/profile_page.dart
@@ -541,7 +541,7 @@ class ProfilePage extends StatelessWidget {
                   child: Column(
                     children: [
                       Text(
-                        'App Version: 3.0.3.35',
+                        'App Version: 3.0.3.36',
                         style: TextStyle(
                           fontSize: 17,
                           fontWeight: FontWeight.w500,
